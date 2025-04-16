d427cf4e7b9053960d14b270f8899d75d1135c67
Alvaro Placido <164355474+AlvaroP2003@users.noreply.github.com>
2025-04-16 16:54:41 +0200

    changes

--- src/assets/projects-data.js
+++ src/assets/projects-data.js
@@ -11,7 +11,20 @@ const projectsData = [
             './Icons/javascript-svgrepo-com.svg',
             './Icons/netlify-svgrepo-com.svg'
         ]
-    }
+    },
+    {
+        name : "Farmer's Market Langing Page",
+        description : 'A clean, responsive landing page designed for a local farmer’s market. This project highlights fresh produce, community events, and vendor information with a modern, user-friendly layout. Built using HTML, CSS, and JavaScript (or React, if you used it), it focuses on clear navigation, engaging visuals, and a warm, organic feel to reflect the market’s personality.',
+        thumbnail : '/Images/project_podify-01.png',
+        video : './Videos/projects_podify.mkv',
+        link : 'https://podifypodcastlistener.netlify.app',
+        tech : [
+            './Icons/html-5-svgrepo-com.svg',
+            './Icons/css-3-svgrepo-com.svg',
+            './Icons/react-svgrepo-com.svg',
+            './Icons/netlify-svgrepo-com.svg'
+        ]
+    },
 ]
 
 export default projectsData
