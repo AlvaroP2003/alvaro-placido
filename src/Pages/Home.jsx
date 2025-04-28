4fb77974a6e4fbfdf381a063239edba01b8589ff
Alvaro Placido <164355474+AlvaroP2003@users.noreply.github.com>
2025-04-28 16:28:18 +0200

    Chamges

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,11 +2,8 @@ import {React,useState} from "react";
 import Loading from "./Loading";
 
 export default function Home() {
-    const [loading,setLoading] = useState(false)
 
     return (
-        <>
-        {loading ? }
         <section className="home">
             <h1>ALVARO PLACIDO</h1>
             <h2>SOFTWARE DEVELOPER</h2>
@@ -14,6 +11,5 @@ export default function Home() {
             — Linus Torvalds (Creator of Linux)</h3>
             <button className="action-btn" onClick={() => window.location.href = "mailto:placido.alvaro03@gmail.com"}>Get in Touch</button>
         </section>
-        </>
     )
 }
--- src/Pages/Loading.jsx
+++ src/Pages/Loading.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Loading() {
     return (
-        <section>Loading</section>
+        <section>Loading...</section>
     )
 }
